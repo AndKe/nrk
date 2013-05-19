c14e8170ae5d52c16e4cb8e04088997d918a10c6
datagutten <datagutten@datagutten.net>
2013-05-19 15:40:36 +0200

    Fjernet gammel kode fra serie.php og fjernet overflødige script
    get.php er integrert i functions.php og subripper.php kan erstattes av
    nrkdl.php og serie.php

--- serie.php
+++ serie.php
@@ -14,10 +14,10 @@ foreach($serier as $url)
 	{
 		preg_match('^(.+) .+^',$sesonger[0]['titler'][0],$serietittel);
 		$serietittel=html_entity_decode($serietittel[1]);
-		$outpath=$nrk->config['outpath'].$nrk->filnavn($serietittel.' '.$sesong['sesongtittel']).'/';
+		$outpath=$nrk->config['outpath'].$nrk->filnavn($serietittel.' '.$sesong['sesongtittel']).'/'; //Lag mappenavn for sesongen
 		if(isset($unntak) && array_search($serietittel.' '.$sesong['sesongtittel'],$unntak)!==false) //Sjekk om denne sesongen ikke skal rippes
 			continue;
-		if(!file_exists($outpath))
+		if(!file_exists($outpath)) //Lag mappe
 			mkdir($outpath,0777,true);	
 		foreach ($sesong['url'] as $episodekey=>$url) //Gå gjennom episodene i sesongen
 		{
@@ -26,55 +26,7 @@ foreach($serier as $url)
 			var_dump($nrk->nrkrip($url,$outpath));
 			echo $nrk->error;
 			echo $nrk->sjekk->error;
-
-			/*die($url."\n");
-			$filnavn=$nrk->filnavn($sesong['titler'][$episodekey]);
-			$tsfil=$outpath.$filnavn.'.ts';
-			$mkvfil=$outpath.$filnavn.'.mkv';
-			$episodedata=$nrk->get($sesong['url'][$episodekey]);
-			if(file_exists($outpath.$filnavn.'.mkv')) //Sjekk om episoden allerede er lastet ned
-			{
-				//echo "eksisterer\n";
-				$nrk->sjekkfil($mkvfil,$nrk->varighet($episodedata));
-				if($nrk->varighetsjekk($episodedata,$mkvfil))
-				{
-					echo html_entity_decode($sesong['titler'][$episodekey])." er allerede lastet ned\n";
-					continue;
-				}
-				else
-				{
-					rename($tsfil,$tsfil.'.old');
-					rename($mkvfil,$mkvfil.'.old');
-				}
-			}
-			elseif(file_exists($tsfil))
-				rename($tsfil,$tsfil.'.old');
-			
-			
-
-			
-			if($segmentlist=segmentlist($episodedata)) //Hvis segmentliste er funnet, last ned
-			{
-				
-				if(!file_exists($outpath))
-					mkdir($outpath);
-				file_put_contents($outpath.$filnavn.'.htm',$episodedata);
-				//echo "Laster ned {$episoder[1][$key]}\n"; //Vis hvilken episode som lastes ned
-				//echo $outfile."\n";
-				//echo $baseurl."\n";
-				//$segmentlist=get($baseurl.'index_4_av.m3u8?e=6303f0e8a89ec6bd&id=',false,$url,$agent); //Hent liste over segmenter
-				
-				download($segmentlist,$outpath.$filnavn); //Last ned episoden
-				subtitle($sesong['id'][$episodekey],$outpath.$filnavn); //Hent undertekst
-			}*/
-			}
-		
-	}
-	
-	unset($mkvfile,$episoder);
+		}
+	}	
 }
-
-
-//print_r($episoder);
-
 ?>
