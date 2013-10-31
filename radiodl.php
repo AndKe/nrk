3056418e220aac1b4845eefa0d1f048acfc549d8
datagutten <datagutten@datagutten.net>
2013-10-31 21:24:25 +0100

    Filnavn og muxing

--- radiodl.php
+++ radiodl.php
@@ -2,12 +2,16 @@
 unset($argv[0]);
 chdir(dirname(realpath(__FILE__))); //Bytt til mappen scriptet ligger i så relative filbaner blir riktige
 require 'functions.php';
+$nrk->dependcheck->depend(array('avconv'));
 $nrk=new nrkripper;
 $data=$nrk->get($argv[1]);
 $segments=$nrk->segmentlist($data);
 
 //print_r($result);
 //$segments=$nrk->get($result[1].'index_1_a.m3u8?null=');
-$nrk->downloadts($segments,'radio');
+$title=$nrk->finntittel($data);
+$utfil=$nrk->config['outpath'].$nrk->filnavn($title);
+$nrk->downloadts($segments,$utfil);
+shell_exec("avconv -i $utfil.ts -bsf:a aac_adtstoasc -acodec copy $utfil.m4a");
 
 echo $nrk->error;
